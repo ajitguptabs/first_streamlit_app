acb6d2a6ecfa92bd0e7581f48a3b202d67b2a6c6
ajitguptabs <122227746+ajitguptabs@users.noreply.github.com>
2023-01-09 15:31:06 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit 
 streamlit.title ('MY first program')
 streamlit.title ('it was in github and pythone')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
